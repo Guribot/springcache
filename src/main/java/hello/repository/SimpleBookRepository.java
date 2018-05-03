9c0f13997fc8b1fc730cacea2ecb58b5a99b0720
Kate Spitzer <kspitzer@blueorigin.com>
2018-05-03 10:11:34 -0700

    create basic model, repository simulation

---
+++ src/main/java/hello/repository/SimpleBookRepository.java
@@ -0,0 +1,22 @@
+package hello.repository;
+
+import hello.model.Book;
+
+public class SimpleBookRepository implements BookRepository {
+  
+  @Override
+  public Book getBookByISBN(String isbn) {
+    simulateSlowService();
+    return new Book(isbn, "Some Cool Book");
+  }
+  
+  // simulating latency
+  private void simulateSlowService() {
+    try {
+      long time = 3000L;
+      Thread.sleep(time);
+    } catch (InterruptedException ie) {
+      throw new IllegalStateException();
+    }
+  }
+}
